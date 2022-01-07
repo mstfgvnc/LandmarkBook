ffde7e89de6a5700d3d4224a7200e7bb6f4aa565
Mustafa Güvenç <mustafa_gvnc@windowslive.com>
2022-01-07 21:36:50 +0100

    initial commit

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -10,28 +10,21 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDelegate ,UICollectionViewDataSource	{
   
    
-    
-    
     var landmarkNames = [String]()
     var landmarkImages = [UIImage]()
     var chosenLandmarkName = ""
     var chosenLandmarkImage = UIImage()
 
 
-  //  @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-      //  tableView.delegate = self
-      //  tableView.dataSource = self
-        
+     
         collectionView.delegate = self
         collectionView.dataSource = self
         
 	
-        
         //Landmark Book Data
         
         landmarkNames.append("Colosseum")
@@ -48,39 +41,9 @@ class ViewController: UIViewController, UICollectionViewDelegate ,UICollectionVi
         
        navigationItem.title = "Landmark Book"
         
-    }
-    
-    /*
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            landmarkNames.remove(at: indexPath.row)
-            landmarkImages.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
-        }
-    }
-    
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = landmarkNames[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return landmarkNames.count
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        chosenLandmarkName = landmarkNames[indexPath.row]
-        chosenLandmarkImage = landmarkImages[indexPath.row]
         
-        performSegue(withIdentifier: "toImageViewController", sender: nil)
     }
     
-     */
-    
-  
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return landmarkNames.count
